b9dd5cdd64226425a7627666e3168f5d48c47e21
villela <leonardo.villela37@gmail.com>
2015-04-29 01:12:52 -0300

    Mais ajustes pro heroku

--- src/restrict/server.js
+++ src/restrict/server.js
@@ -23,7 +23,7 @@ io.on('connection', function(socket) {
 app.use(express.static(path.join(__dirname, '../../')));
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.set('port', 3000 || process.env.PORT);
+app.set('port', process.env.PORT || 3000);
 
 var server = http.listen(app.get('port'), function() {
     console.log('Estou escutando na porta ' + app.get('port'));
